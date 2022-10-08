0fe06e5e8c460cdab5881da82fc3f66dd5ba033b
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-10-08 21:06:16 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+
 import './App.css';
+async function loadPage() {
+  //Getting Post content
+    let responsePosts = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let posts = await responsePosts.json();
+
+  // Getting User names
+    let responseUsers = await fetch("https://jsonplaceholder.typicode.com/users");
+    let users = await responseUsers.json();
+  // Gettinf Photos
+   let responsePhotos = await fetch ("https://jsonplaceholder.typicode.com/photos");
+   let photos = await responsePhotos.json();
+  // Getting Comments
+   let responseComments = await fetch ("https://jsonplaceholder.typicode.com/comments");
+   let comments = await responseComments.json();
+   //Add Avatar
+const mappedPosts = posts.map((p) => {
+  const avatars = photos.find((u) => u.id === p.userId); // userId в постах 
+//Add Comments
+  const commentsInPost = comments.find((u) => u.id === p.userId);
+
+// Changing ID
+  const createdBy = users.find((u) => u.id === p.userId);
+  return {
+    ...p, 
+    commentsInPost,
+    avatars,
+    userId: createdBy ? createdBy.username : "Unknown user",
+  }
+});
+console.log(mappedPosts)
+  //Creating Post
+  for (let post of mappedPosts) {
+    const container = document.getElementById("root");
+    const resultedPost = createPost(post);
+    const postWrapper = document.createElement("div");
+    postWrapper.classList.add("inner");
+    postWrapper.innerHTML = resultedPost;
+    container.appendChild(postWrapper);
+  }
+  // Accordion
+
+let acc = document.getElementsByClassName("accordion");
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} 
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  loadPage();
+ 
 }
+
+function createPost(post) {
+  return `
+    <h1>${post.title}</h1>
+    <p>${post.body}<span>${post.userId}<img class="Avatar" src=${post.avatars.thumbnailUrl}></span></p>
+    <button class="accordion">Comments</button>
+      <div class="panel">
+        <p>${post.commentsInPost.body}<span>${post.commentsInPost.name}<img class="Avatar" src=${post.avatars.thumbnailUrl}></span></p></p>
+        
+      </div>
+  `;
+}
+
+
+
 
 export default App;
