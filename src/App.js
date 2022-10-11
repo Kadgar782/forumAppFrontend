ab6adfbed1014f9c3d4b9bfe7faa6798f575b56b
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-10-11 17:24:11 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,28 @@ function App() {
     "title": "accusamus beatae ad facilis cum similique qui sunt",
     "url": "https://via.placeholder.com/600/92c952",
     "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+  },
+  {
+  
+  "userId": "Bret",
+  "id": 1,
+  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+  "commentsInPost": {
+    "postId": 1,
+    "id": 1,
+    "name": "id labore ex et quam laborum",
+    "email": "Eliseo@gardner.biz",
+    "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+  },
+  "avatars": {
+    "albumId": 1,
+    "id": 1,
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
   }
+
 
 
  }]
@@ -33,10 +54,10 @@ function App() {
 
   // Creating Post with JSX
   return (
-    <div className='inner'>
+     <div  className='outer'>
       {mappedPosts.map((post) => {
         return (
-          <div key={post.id}>
+           <div className='inner' key={post.id}>
             <h1>{post.title}</h1>
 
             <p>
