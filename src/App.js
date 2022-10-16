1cdf50a2692634ab200fcbd6c8557ecd889e2bde
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-10-16 16:05:19 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,104 +1,102 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
-const axios = require('axios').default;
+
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const [photos, setPhotos] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [mappedPosts, setMappedPosts] = useState([]);
+
   //Getting Post content
-  async function doGetRequest(url) {
-    let res = await axios.get(url);
-    let data = res.data;
-    console.log(data);
-  }
-  const posts = doGetRequest("https://jsonplaceholder.typicode.com/posts")
-  const users = doGetRequest("https://jsonplaceholder.typicode.com/users")
-  const photos = doGetRequest("https://jsonplaceholder.typicode.com/photos")
-  const comments = doGetRequest("https://jsonplaceholder.typicode.com/comments")
-  console.log(posts)
-
-  const mappedPosts = [{
+  useEffect(() => {
+    // declare the async data fetching function
+    const fetchData = async (url, setData) => {
+      // get the data from the api
+      const response = await fetch(url);
+      // convert the data to json
+      const json = await response.json();
   
-  "userId": "Bret",
-  "id": 1,
-  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-  "commentsInPost": {
-    "postId": 1,
-    "id": 1,
-    "name": "id labore ex et quam laborum",
-    "email": "Eliseo@gardner.biz",
-    "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
-  },
-  "avatars": {
-    "albumId": 1,
-    "id": 1,
-    "title": "accusamus beatae ad facilis cum similique qui sunt",
-    "url": "https://via.placeholder.com/600/92c952",
-    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
-  },
-  {
+      // set state with the result
+      setData(json);
+    }
   
-  "userId": "Bret",
-  "id": 1,
-  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-  "commentsInPost": {
-    "postId": 1,
-    "id": 1,
-    "name": "id labore ex et quam laborum",
-    "email": "Eliseo@gardner.biz",
-    "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
-  },
-  "avatars": {
-    "albumId": 1,
-    "id": 1,
-    "title": "accusamus beatae ad facilis cum similique qui sunt",
-    "url": "https://via.placeholder.com/600/92c952",
-    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
-  }
+    // call the function
+    
+    fetchData("https://jsonplaceholder.typicode.com/posts",setPosts);
+    fetchData("https://jsonplaceholder.typicode.com/photos",setPhotos)
+    fetchData("https://jsonplaceholder.typicode.com/users",setUsers)
+    fetchData("https://jsonplaceholder.typicode.com/comments",setComments)
+         // make sure to catch any error
+         .catch(console.error);
+    setMappedPosts(
+          posts.map((p) => {
+            const avatars = photos.find((u) => u.id === p.userId); // userId в постах
+            //Add Comments
+            const commentsInPost = comments.find((u) => u.id === p.userId);
+    
+            // Changing ID
+            const createdBy = users.find((u) => u.id === p.userId);
+            return {
+              ...p,
+              commentsInPost,
+              avatars,
+              userId: createdBy ? createdBy.username : "Unknown user"
+            };
+          })
+        );
+    
+        setIsLoading(false);
+  }, [])
 
+  const [isActive, setActive] = useState("false");
 
-
- }]
-
- 
-
-  console.log(mappedPosts);
-  
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
 
   // Creating Post with JSX
   return (
-     <div  className='outer'>
-      {mappedPosts.map((post) => {
-        return (
-           <div className='inner' key={post.id}>
-            <h1>{post.title}</h1>
+    <div className="outer">
+      {isLoading ? (
+        <div>IS loading...</div>
+      ) : (
+        mappedPosts.map((post) => {
+          return (
+            <div className="inner" key={post.id}>
+              <h1>{post.title}</h1>
 
-            <p>
-              {post.body}
-              <span>
-                {post.userId}
-                <img className ="Avatar" src={post.avatars.thumbnailUrl} />
-              </span>
-            </p>
-            <button  className ="accordion">
-              Comments
-            </button>
-            <div className ="panel">
               <p>
-                {post.commentsInPost.body}
+                {post.body}
                 <span>
-                  {post.commentsInPost.name}
-                  <img className ="Avatar" src={post.avatars.thumbnailUrl} />
+                  {post.userId}
+                  <img className="Avatar" src={post.avatars.thumbnailUrl} />
                 </span>
               </p>
+              <button
+                className={isActive ? "accordion" : "accordion active"}
+                onClick={handleToggle}
+              >
+                Comments
+              </button>
+              <div className="panel">
+                <p>
+                  {post.commentsInPost.body}
+                  <span>
+                    {post.commentsInPost.name}
+                    <img className="Avatar" src={post.avatars.thumbnailUrl} />
+                  </span>
+                </p>
+              </div>
             </div>
-
-     
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </div>
   );
 }
 export default App;
+
 
