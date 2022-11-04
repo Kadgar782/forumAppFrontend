7b6fbcc5aaa2669a20423d1c2ee25aea43b45c4a
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-11-04 19:10:04 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import  {MuiAccordion}  from "./MUIComponents/MUIAccordion.tsx";
 import {PostFields} from "./MUIComponents/CreatePost.tsx";
 
 import './App.css';
-import { getValue } from "@testing-library/user-event/dist/utils";
+
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -21,41 +21,39 @@ function App() {
 
       // set state with the result
       return json;
-    }
+    };
     // call the function
     const mapPosts = async () => {
       const [posts, photos, users, comments] = await Promise.all([
         fetchData("https://jsonplaceholder.typicode.com/posts"),
         fetchData("https://jsonplaceholder.typicode.com/photos"),
         fetchData("https://jsonplaceholder.typicode.com/users"),
-        fetchData("https://jsonplaceholder.typicode.com/comments")]);
+        fetchData("https://jsonplaceholder.typicode.com/comments"),
+      ]);
 
-        setMappedPosts(
-          posts.map((p) => {
-            const avatars = photos.find((u) => u.id === p.userId); // userId в постах
-            //Add Comments
-            const commentsInPost = comments.find((u) => u.id === p.userId);
-  
-            // Changing ID
-            const createdBy = users.find((u) => u.id === p.userId);
-            return {
-              ...p,
-              commentsInPost,
-              avatars,
-              userId: createdBy ? createdBy.username : "Unknown user"
-            };
-  
-          })
-        );
-      }
-      mapPosts().then(() => setIsLoading(false))
-    }, [])
+      setMappedPosts(
+        posts.map((p) => {
+          const avatars = photos.find((u) => u.id === p.userId); // userId в постах
+          //Add Comments
+          const commentsInPost = comments.find((u) => u.id === p.userId);
+
+          // Changing ID
+          const createdBy = users.find((u) => u.id === p.userId);
+          return {
+            ...p,
+            commentsInPost,
+            avatars,
+            userId: createdBy ? createdBy.username : "Unknown user",
+          };
+        })
+      );
+    };
+    mapPosts().then(() => setIsLoading(false));
+  }, []);
 
   //Post remove function
   const removeElement = (id) => {
-    const newPosts = mappedPosts.filter(
-      (mappedPosts) => mappedPosts.id !== id
-    );
+    const newPosts = mappedPosts.filter((mappedPosts) => mappedPosts.id !== id);
     setMappedPosts(newPosts);
   };
   //Modal open
@@ -66,10 +64,9 @@ function App() {
   // Creating Post with JSX
   return (
     <div className="outer">
-     <Button onClick={handleOpen}>Open modal</Button>
-     <Modal   open={open} onClose={handleClose}>
-     <PostFields />
-
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal open={open} onClose={handleClose}>
+        <PostFields />
       </Modal>
 
       {isLoading ? (
@@ -77,34 +74,40 @@ function App() {
       ) : (
         mappedPosts.map((post) => {
           return (
-              <div className="inner" key={post.id} id={post.id}>
-              <Typography variant="h5">{post.title}
-              <IconButton   aria-label="delete" disableRipple onClick={()=>removeElement(post.id)} >
-              <DeleteIcon/>
-             </IconButton>
-             </Typography>
-              <p>
-                {post.body}
-                </p>
-                <span>
-                <Avatar   alt="Placeholder" src={post.avatars.thumbnailUrl} variant="rounded" 
-                sx={{
-                maxWidth: 35,
-                maxHeight: 35,
-                marginRight: 0.5
-                }}/>
-                  {post.userId}
-                </span>
-                
-              <Divider
-              sx={{border: 1}} />
-             <MuiAccordion header={"Comments"} 
-             content={post.commentsInPost.body} 
-             creatorAvatar={post.avatars.thumbnailUrl} 
-             creatorName={post.commentsInPost.name}
-             />
+            <div className="inner" key={post.id} id={post.id}>
+              <Typography variant="h5">
+                {post.title}
+                <IconButton
+                  aria-label="delete"
+                  disableRipple
+                  onClick={() => removeElement(post.id)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </Typography>
+              <p>{post.body}</p>
+              <span>
+                <Avatar
+                  alt="Placeholder"
+                  src={post.avatars.thumbnailUrl}
+                  variant="rounded"
+                  sx={{
+                    maxWidth: 35,
+                    maxHeight: 35,
+                    marginRight: 0.5,
+                  }}
+                />
+                {post.userId}
+              </span>
+
+              <Divider sx={{ border: 1 }} />
+              <MuiAccordion
+                header={"Comments"}
+                content={post.commentsInPost.body}
+                creatorAvatar={post.avatars.thumbnailUrl}
+                creatorName={post.commentsInPost.name}
+              />
             </div>
-            
           );
         })
       )}
@@ -112,3 +115,4 @@ function App() {
   );
 }
 export default App;
+
