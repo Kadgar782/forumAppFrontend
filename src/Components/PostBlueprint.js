b65a90d71ced94f4912d450325cf5e49509f561e
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-12-23 18:19:48 +0600

    Update PostBlueprint.js

--- src/Components/PostBlueprint.js
+++ src/Components/PostBlueprint.js
@@ -1,56 +1,52 @@
-import {  Typography,Avatar,IconButton, } from "@mui/material";
+import { Typography, Avatar, IconButton } from "@mui/material";
 import { CommentSchema } from "./CommentBlueprint";
 import Divider from "@mui/material";
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import React from 'react';
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import React from "react";
 
+export const PostSchema = ({ arrayWithPosts, checkingId, deleteElement }) => {
+  return arrayWithPosts.map((post) => {
+    console.log(post);
+    return (
+      <div className="inner" key={post._id}>
+        <Typography variant="h5">
+          {post.title}
 
-export const PostSchema = ({arrayWithPosts,checkingId,deleteElement}) =>{
-  return(
-    arrayWithPosts.map((post) => {
-      console.log(post);
-        return (
-          <div className="inner" key={post._id}>
-            <Typography variant="h5">
-              {post.title}
+          <IconButton
+            aria-label="Edit"
+            disableRipple
+            id={post._id}
+            onClick={checkingId}
+          >
+            <EditIcon />
+          </IconButton>
 
-              <IconButton
-                aria-label="Edit"
-                disableRipple
-                id={post._id}
-                onClick={checkingId}
-              >
-                <EditIcon />
-              </IconButton>
+          <IconButton
+            aria-label="delete"
+            disableRipple
+            onClick={() => deleteElement(post._id)}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </Typography>
+        <p>{post.body}</p>
 
-              <IconButton
-                aria-label="delete"
-                disableRipple
-                onClick={() => deleteElement(post._id)}
-              >
-                <DeleteIcon />
-              </IconButton>
-            </Typography>
-            <p>{post.body}</p>
-
-            <span>
-              <Avatar
-                alt="Placeholder"
-                src={post.thumbnailUrl}
-                variant="rounded"
-                sx={{
-                  maxWidth: 35,
-                  maxHeight: 35,
-                  marginRight: 0.5,
-                }}
-              />
-              {post.userId}
-            </span>
- 
-            </div>
-        
-  );
-})
-)
-}
+        <span>
+          <Avatar
+            alt="Placeholder"
+            src={post.thumbnailUrl}
+            variant="rounded"
+            sx={{
+              maxWidth: 35,
+              maxHeight: 35,
+              marginRight: 0.5,
+            }}
+          />
+          {post.userId}
+        </span>
+        <CommentSchema wholePost={post} />
+      </div>
+    );
+  });
+};
