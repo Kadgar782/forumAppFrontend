86d8f28104ba0707ddd64b9125258898c25b5c4c
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-12-23 18:18:57 +0600

    Update CommentBlueprint.js

--- src/Components/CommentBlueprint.js
+++ src/Components/CommentBlueprint.js
@@ -1,27 +1,17 @@
-import  {MuiAccordion}  from "./MUIAccordion.js";
-import React from 'react';
+import { MuiAccordion } from "./MUIAccordion.js";
+import React from "react";
 
-
-export const CommentSchema = ({arrayWithPosts}) => {
-
-    return(
-        arrayWithPosts.map((post) => {
-          console.log(post);
-          if (post.commentsInPost === 0 ||
-            post.commentsInPost === undefined)
-            return ( 
-              <MuiAccordion header={"Comments"} content="No comments yet" />  
-            );
-            else 
-            return (
-              <MuiAccordion
-              header={"Comments"}
-              content={post.commentsInPost.body}
-              creatorAvatar={post.thumbnailUrl}
-              creatorName={post.commentsInPost.name}
-            />
-            )
-        })
-        )
-
-}
+export const CommentSchema = ({ wholePost }) => {
+  console.log(wholePost);
+  if (wholePost.commentsInPost === 0 || wholePost.commentsInPost === undefined)
+    return <MuiAccordion header={"Comments"} content="No comments yet" />;
+  else
+    return (
+      <MuiAccordion
+        header={"Comments"}
+        content={wholePost.commentsInPost.body}
+        creatorAvatar={wholePost.thumbnailUrl}
+        creatorName={wholePost.commentsInPost.name}
+      />
+    );
+};
