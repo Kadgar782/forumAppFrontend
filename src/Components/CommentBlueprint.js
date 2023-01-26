33c6facfc9ddd7f10875666ad5fbf76dd1292e31
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2023-01-26 14:20:47 +0600

    Add files via upload

--- src/Components/CommentBlueprint.js
+++ src/Components/CommentBlueprint.js
@@ -2,17 +2,21 @@ import { MuiAccordion } from "./MUIAccordion.js";
 import React from "react";
 
 export const CommentSchema = ({ wholePost }) => {
-  if (wholePost.commentsInPost === 0 || wholePost.commentsInPost === undefined)
-    return ( 
+  if (wholePost.commentsInPost === 0 || wholePost.commentsInPost === undefined || wholePost.commentsInPost === null )
+    return (
     <MuiAccordion header={"Comments"} content="No comments yet" /> 
     );  
   else
-    return (   
+  console.log(wholePost)
+    return wholePost.map((post)=>{
+      return (   
       <MuiAccordion
         header={"Comments"}
-        content={wholePost.commentsInPost.body}
-        creatorAvatar={wholePost.thumbnailUrl}
-        creatorName={wholePost.commentsInPost.name}
+        content={post.commentsInPost.body}
+        creatorAvatar={post.thumbnailUrl}
+        creatorName={post.commentsInPost.name}
       />
-    );
+      );     
+    });
+
 };
